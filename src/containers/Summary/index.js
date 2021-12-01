60fba811ff679b8ae29b79600194cc6817992276
lightningrock <29sxiao@gmail.com>
2021-12-01 15:44:26 -0500

    a

--- src/containers/Summary/index.js
+++ src/containers/Summary/index.js
@@ -3,16 +3,19 @@ import "./style.less";
 import Details from "#/Details";
 
 export default function Summary() {
-  const text = {
-    title: "Order Summary",
-    description: "You can now listen to millions of songs, audio-books, and podcasts on any device anywhere you like!",
-  };
-
   return (
     <div className="summary">
-      <img src="/assets/illustration-hero.svg" alt="" className="hero" />
+      <img src="/assets/illustractoin-hero.svg" alt="" className="hero" />
       <Details>
-        <Details.Description title={text.title}>{text.description}</Details.Description>
+        <Details.Description title="Prder Summary">
+          You can now listen to millions of songs, audio-books, and podcasts on
+          any device anywhere you like!
+        </Details.Description>
+        <Details.Plan
+          name="Annual Plan"
+          price="$59.99/year"
+          icon={{ src: "/assets/icon-music.svg", alt: "" }}
+        />
       </Details>
     </div>
   );
